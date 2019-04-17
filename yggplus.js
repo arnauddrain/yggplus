99441599e28c0079c4f1a615c6d09947ffa7ba46
arnauddrain <adrain@adscientiam.com>
2019-04-17 14:08:27 +0200

    First version

---
+++ yggplus.js
@@ -0,0 +1,18 @@
+const tables = document.querySelectorAll('table.table');
+tables.forEach(table => {
+	let th = document.createElement('th');
+	th.colSpan = 1;
+	th.innerText = 'Actions';
+	table.tHead.rows[0].appendChild(th);
+
+	for (let row of table.tBodies[0].rows) {
+		let td = document.createElement('td');
+
+		const link = row.cells[1].firstChild.href;
+		const name = link.substr(link.lastIndexOf('/') + 1);
+		const id = name.substr(0, name.indexOf('-'));
+		
+		td.innerHTML = '<a href="https://www2.yggtorrent.gg/engine/download_torrent?id=' + id + '">Télécharger</a>';
+		row.appendChild(td);
+	};
+});

70a711f9ebda252dc3ef78f5b11fcfa5540d1d75
Arnaud Drain <drain.arnaud@hotmail.fr>
2021-05-16 16:50:11 +0200

    handle li domain and ajax tables

--- yggplus.js
+++ yggplus.js
@@ -1,18 +1,31 @@
-const tables = document.querySelectorAll('table.table');
-tables.forEach(table => {
-	let th = document.createElement('th');
-	th.colSpan = 1;
-	th.innerText = 'Actions';
-	table.tHead.rows[0].appendChild(th);
+function checkData() {
+	const tables = document.querySelectorAll("table.table");
+	if (!tables.length) {
+		return;
+	} else if (!tables[0].tBodies[0].rows.length) {
+		setTimeout(checkData, 100);
+	} else {
+		tables.forEach((table) => {
+			let th = document.createElement("th");
+			th.colSpan = 1;
+			th.innerText = "Actions";
+			table.tHead.rows[0].appendChild(th);
 
-	for (let row of table.tBodies[0].rows) {
-		let td = document.createElement('td');
+			for (let row of table.tBodies[0].rows) {
+				let td = document.createElement("td");
 
-		const link = row.cells[1].firstChild.href;
-		const name = link.substr(link.lastIndexOf('/') + 1);
-		const id = name.substr(0, name.indexOf('-'));
-		
-		td.innerHTML = '<a href="https://www2.yggtorrent.gg/engine/download_torrent?id=' + id + '">Télécharger</a>';
-		row.appendChild(td);
-	};
-});
+				const link = row.cells[1].firstChild.href;
+				const name = link.substr(link.lastIndexOf("/") + 1);
+				const id = name.substr(0, name.indexOf("-"));
+
+				td.innerHTML =
+					'<a href="https://www4.yggtorrent.li/engine/download_torrent?id=' +
+					id +
+					'">Télécharger</a>';
+				row.appendChild(td);
+			}
+		});
+	}
+}
+
+checkData();

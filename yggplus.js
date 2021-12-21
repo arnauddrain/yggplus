4ea3bc17a5fd18d31923f8bd576a5ba69f852b37
Arnaud Drain <arnaud.drain@phantombuster.com>
2021-12-21 10:44:56 +0100

    prettify project

--- yggplus.js
+++ yggplus.js
@@ -1,31 +1,31 @@
 function checkData() {
-	const tables = document.querySelectorAll("table.table");
-	if (!tables.length) {
-		return;
-	} else if (!tables[0].tBodies[0].rows.length) {
-		setTimeout(checkData, 100);
-	} else {
-		tables.forEach((table) => {
-			let th = document.createElement("th");
-			th.colSpan = 1;
-			th.innerText = "Actions";
-			table.tHead.rows[0].appendChild(th);
+  const tables = document.querySelectorAll("table.table");
+  if (!tables.length) {
+    return;
+  } else if (!tables[0].tBodies[0].rows.length) {
+    setTimeout(checkData, 100);
+  } else {
+    tables.forEach((table) => {
+      let th = document.createElement("th");
+      th.colSpan = 1;
+      th.innerText = "Actions";
+      table.tHead.rows[0].appendChild(th);
 
-			for (let row of table.tBodies[0].rows) {
-				let td = document.createElement("td");
+      for (let row of table.tBodies[0].rows) {
+        let td = document.createElement("td");
 
-				const link = row.cells[1].firstChild.href;
-				const name = link.substr(link.lastIndexOf("/") + 1);
-				const id = name.substr(0, name.indexOf("-"));
+        const link = row.cells[1].firstChild.href;
+        const name = link.substr(link.lastIndexOf("/") + 1);
+        const id = name.substr(0, name.indexOf("-"));
 
-				td.innerHTML =
-					'<a href="https://www3.yggtorrent.re/engine/download_torrent?id=' +
-					id +
-					'">Télécharger</a>';
-				row.appendChild(td);
-			}
-		});
-	}
+        td.innerHTML =
+          '<a href="https://www3.yggtorrent.re/engine/download_torrent?id=' +
+          id +
+          '">Télécharger</a>';
+        row.appendChild(td);
+      }
+    });
+  }
 }
 
 checkData();
